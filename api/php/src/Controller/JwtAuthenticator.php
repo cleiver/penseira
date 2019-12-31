c817ea93f0b121cbd6de9be55bfd12509c234321
cleiver <cleiver@gmail.com>
2019-12-30 23:00:33 -0300

    Documenting JWT authenticator so I remember easily in the future 😜

--- api/php/src/Controller/JwtAuthenticator.php
+++ api/php/src/Controller/JwtAuthenticator.php
@@ -25,17 +25,53 @@ class JwtAuthenticator extends AbstractGuardAuthenticator
     $this->userRepository = $userRepository;
   }
 
-  public function start(Request $request, ?AuthenticationException $authException = null)
+  /**
+   * Returns a response that directs the user to authenticate.
+   *
+   * This is called when an anonymous request accesses a resource that
+   * requires authentication. The job of this method is to return some
+   * response that "helps" the user start into the authentication process.
+   *
+   * Examples:
+   *
+   * - For a form login, you might redirect to the login page
+   *
+   *     return new RedirectResponse('/login');
+   *
+   * - For an API token authentication system, you return a 401 response
+   *
+   *     return new Response('Auth header required', 401);
+   *
+   * @return Response
+   */
+  public function start(Request $request, ?AuthenticationException $authException = null): Response
   {
-    # code...
+    return new Response('Auth header required', Response::HTTP_UNAUTHORIZED);
   }
 
+  /**
+   * Does the authenticator support the given Request?
+   *
+   * If this returns false, the authenticator will be skipped.
+   *
+   * @return bool
+   */
   public function supports(Request $request): bool
   {
     return '/login' !== $request->getPathInfo();
   }
 
-  public function getCredentials(Request $request): ?object
+  /**
+   * Get the authentication credentials from the request and return them
+   * as any type (e.g. an associate array).
+   *
+   * Whatever value you return here will be passed to getUser() and checkCredentials()
+   *
+   * @return mixed Any non-null value
+   *
+   * @throws \UnexpectedValueException If null is returned
+   */
+  public function getCredentials(Request $request)
   {
     $token = str_replace('Bearer ', '', $request->headers->get('Authorization'));
 
@@ -46,6 +82,20 @@ class JwtAuthenticator extends AbstractGuardAuthenticator
     }
   }
 
+  /**
+   * Return a UserInterface object based on the credentials.
+   *
+   * The *credentials* are the return value from getCredentials()
+   *
+   * You may throw an AuthenticationException if you wish. If you return
+   * null, then a UsernameNotFoundException is thrown for you.
+   *
+   * @param mixed $credentials
+   *
+   * @throws AuthenticationException
+   *
+   * @return UserInterface|null
+   */
   public function getUser($credentials, UserProviderInterface $userProvider): ?UserInterface
   {
     if (!is_object($credentials) || !property_exists($credentials, 'email')) {
@@ -55,11 +105,36 @@ class JwtAuthenticator extends AbstractGuardAuthenticator
     return $this->userRepository->findOneBy(['email' => $credentials->email]);
   }
 
+  /**
+   * Returns true if the credentials are valid.
+   *
+   * If false is returned, authentication will fail. You may also throw
+   * an AuthenticationException if you wish to cause authentication to fail.
+   *
+   * The *credentials* are the return value from getCredentials()
+   *
+   * @param mixed $credentials
+   *
+   * @return bool
+   *
+   * @throws AuthenticationException
+   */
   public function checkCredentials($credentials, UserInterface $user): bool
   {
     return is_object($credentials) && property_exists($credentials, 'email');
   }
 
+  /**
+   * Called when authentication executed, but failed (e.g. wrong username password).
+   *
+   * This should return the Response sent back to the user, like a
+   * RedirectResponse to the login page or a 403 response.
+   *
+   * If you return null, the request will continue, but the user will
+   * not be authenticated. This is probably not what you want to do.
+   *
+   * @return Response|null
+   */
   public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
   {
     return new JsonResponse(
@@ -70,11 +145,36 @@ class JwtAuthenticator extends AbstractGuardAuthenticator
     );
   }
 
+  /**
+   * Called when authentication executed and was successful!
+   *
+   * This should return the Response sent back to the user, like a
+   * RedirectResponse to the last page they visited.
+   *
+   * If you return null, the current request will continue, and the user
+   * will be authenticated. This makes sense, for example, with an API.
+   *
+   * @return Response|null
+   */
   public function onAuthenticationSuccess(Request $request, TokenInterface $token, String $providerKey): ?Response
   {
     return null;
   }
 
+  /**
+   * Does this method support remember me cookies?
+   *
+   * Remember me cookie will be set if *all* of the following are met:
+   *  A) This method returns true
+   *  B) The remember_me key under your firewall is configured
+   *  C) The "remember me" functionality is activated. This is usually
+   *      done by having a _remember_me checkbox in your form, but
+   *      can be configured by the "always_remember_me" and "remember_me_parameter"
+   *      parameters under the "remember_me" firewall key
+   *  D) The onAuthenticationSuccess method returns a Response object
+   *
+   * @return bool
+   */
   public function supportsRememberMe(): bool
   {
     return false;
